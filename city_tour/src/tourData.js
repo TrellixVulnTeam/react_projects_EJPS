c2060f8409c8eb9e1082713e5dfeb6de8cd726c3
SQuinones <78192383+SQuinones@users.noreply.github.com>
2022-01-20 21:44:53 -0500

    adding react projects creating in TTP program

---
+++ city_tour/src/tourData.js
@@ -0,0 +1,50 @@
+export const tourData = [
+    {
+        id: 1,
+        city:"new york",
+        img: "./img/newyork.jpg",
+        name: "Statue of Liberty tour",
+        info:
+            "Lorem"            
+    },
+    {
+        id: 2,
+        city:"paris",
+        img: "./img/paris.jpg",
+        name: "paris lights tour",
+        info:
+            "Lorem"   
+    },
+    {
+        id: 3,
+        city:"London",
+        img: "./img/London.jpg",
+        name: "London Royal Palace tour",
+        info:
+            "Lorem"   
+    },
+    {
+        id: 4,
+        city:"tokyo",
+        img: "./img/tokyo.jpg",
+        name: "tokyo sushi  tour",
+        info:
+            "Lorem"   
+    },
+    {
+        id: 5,
+        city:"Brooklyn Bridge",
+        img: "./img/bkbridgr.jpg",
+        name: "Brooklyn Bridge tour",
+        info:
+            "Lorem"   
+    },
+    {
+        id: 6,
+        city:"Yankee Stadium",
+        img: "./img/yankee.jpg",
+        name: "Yankee Stadium tour",
+        info:
+            "Lorem"   
+    },    
+];
